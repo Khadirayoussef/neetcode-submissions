09a67423cd0a78c534dc82870f24402da0063edc
Youssef Khadira <149430217+Khadirayoussef@users.noreply.github.com>
2026-08-12 16:56:55 +0100

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        boolean[] grouped = new boolean[strs.length];
+
+        for (int i = 0; i < strs.length; i++) {
+            if (grouped[i]) {
+                continue;
+            }
+
+            List<String> currentWork = new ArrayList();
+            currentWork.add(strs[i]);
+            grouped[i] = true;
+
+
+            for (int j = i + 1; j < strs.length; j++) {
+                if (grouped[j]) {
+                    continue;
+                }
+
+                if (strs[i].length() != strs[j].length())
+                    continue;
+
+                int[] alphabetCounts = new int[26];
+                for (int l = 0; l < strs[i].length(); l++) {
+                    alphabetCounts[strs[i].charAt(l) - 'a']++;
+                    alphabetCounts[strs[j].charAt(l) - 'a']--; 
+                }
+                boolean flag = true;
+                for (int count : alphabetCounts) {
+                    if (count != 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    currentWork.add(strs[j]);
+                    grouped[j] = true;
+                }
+            }
+            result.add(currentWork);
+        }
+        return result;
+    }
+}
